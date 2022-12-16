2d8ed4bae25f284d62a920d2e10c87b4626a13a1
pablogarciamoralez <pablogarciamoralez@gmail.com>
2022-12-16 13:29:35 -0300

    swiping images added

--- src/Components/DealDetail.js
+++ src/Components/DealDetail.js
@@ -1,10 +1,60 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  PanResponder,
+  Animated,
+  Dimensions,
+} from "react-native";
 import { priceDisplay } from "../Utils/functions";
 import ajax from "./ajax";
 
 export default class DealDetail extends React.Component {
+  imageXPos = new Animated.Value(0);
+  imagePanResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, gs) => {
+      this.imageXPos.setValue(gs.dx);
+    },
+    onPanResponderRelease: (evt, gs) => {
+      this.cellWidth = Dimensions.get("window").width;
+      if (Math.abs(gs.dx) > this.cellWidth * 0.4) {
+        const direction = Math.sign(gs.dx);
+        Animated.timing(this.imageXPos, {
+          toValue: direction * this.cellWidth,
+          duration: 250,
+        }).start(() => this.handleSwipe(-1 * direction));
+      } else {
+        Animated.spring(this.imageXPos, {
+          toValue: 0,
+        }).start();
+      }
+    },
+  });
+
+  handleSwipe = (indexDirection) => {
+    if (!this.state.deal.media[this.state.imageIndex + indexDirection]) {
+      Animated.spring(this.imageXPos, {
+        toValue: 0,
+      }).start();
+      return;
+    }
+    this.setState(
+      (prevState) => ({
+        imageIndex: prevState.imageIndex + indexDirection,
+      }),
+      () => {
+        this.imageXPos.setValue(indexDirection * this.cellWidth);
+        Animated.spring(this.imageXPos, {
+          toValue: 0,
+        }).start();
+      }
+    );
+  };
   static propTypes = {
     initialDealData: PropTypes.object.isRequired,
     onBack: PropTypes.func.isRequired,
@@ -12,6 +62,7 @@ export default class DealDetail extends React.Component {
 
   state = {
     deal: this.props.initialDealData,
+    imageIndex: 0,
   };
 
   async componentDidMount() {
@@ -28,7 +79,11 @@ export default class DealDetail extends React.Component {
         <TouchableOpacity onPress={this.props.onBack}>
           <Text style={styles.backLink}>Back</Text>
         </TouchableOpacity>
-        <Image source={{ uri: deal.media[0] }} style={styles.image} />
+        <Animated.Image
+          {...this.imagePanResponder.panHandlers}
+          source={{ uri: deal.media[this.state.imageIndex] }}
+          style={[{ left: this.imageXPos }, styles.image]}
+        />
         <View style={styles.info}>
           <Text style={styles.title}>{deal.title}</Text>
           <View style={styles.footer}>
